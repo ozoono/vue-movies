05a441fba95bb5c9efa980fd84a3f876657fc15e
David <david@ozoono.com>
2019-10-13 23:04:18 +0200

    update

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-export const key = '5fd51bf2e1f0d2c7ef0470a0e4788414'
+export const key = '<YOUR_API_KEY>'
 export const API = axios.create({
 	baseURL: `https://api.themoviedb.org/3/`
 })
